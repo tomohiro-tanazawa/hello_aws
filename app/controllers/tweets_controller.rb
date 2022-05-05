364a605cde4bf7a4ff74237fca4b700c911dce31
tanazawa_aws <tanabota5525@gmail.com>
2022-05-05 02:49:42 +0000

    complete_to_create_user_and_tweet_page

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,6 +4,13 @@ class TweetsController < ApplicationController
     @tweet = Tweet.new # 新規投稿用の空のインスタンス
   end
 
+  def create
+    @tweet = Tweet.new(tweet_params)  # フォームから送られてきたデータ(body)をストロングパラメータを経由して@tweetに代入
+    @tweet.user_id = current_user.id # user_idの情報はフォームからはきていないので、deviseのメソッドを使って「ログインしている自分のid」を代入
+    @tweet.save
+    redirect_to tweets_path
+  end
+  
   def index
     @tweets = Tweet.all
   end
@@ -12,6 +19,8 @@ class TweetsController < ApplicationController
     @tweet = Tweet.find(params[:id])
   end
 
-  def create
-  end
+  private
+    def tweet_params
+      params.require(:tweet).permit(:body) # tweetモデルのカラムのみを許可
+    end
 end
