b77a4660939d67846ac05440b798ac28fec6b61b
tanazawa_aws <tanabota5525@gmail.com>
2022-05-05 11:59:35 +0000

    complete to create favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  has_many :tweets  # これを追加
+  has_many :tweets
+  has_many :favorites
+  has_many :favorite_tweets, through: :favorites, source: :tweet
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
    root 'tweets#index' 
   
     devise_for :users
-    resources :tweets  # 追加
-    resources :users  # 追加
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    
+    resources :tweets do
+      resource :favorites, only: [:create, :destroy]
+    end
+    resources :users
 end
