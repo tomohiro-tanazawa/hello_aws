5c11558d0db6a88966c8a7fddaf56bfeb28bb5a0
tanazawa_aws <tanabota5525@gmail.com>
2022-05-12 06:56:03 +0000

    comment_incomplete0512

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,18 @@
 class CommentsController < ApplicationController
   def create
+    @tweet = Tweet.find(params[:id])
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    @comment.tweet_id = @tweet.id
+    comments.save
+    redirect_to tweet_path(@tweet.id)
   end
 
   def destroy
   end
+  
+  private
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
 end
